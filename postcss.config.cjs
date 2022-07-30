8c19e8b5b2365ec3e6ad18fa5445527ccdee9814
Quinten Coret <Qcoret@gmail.com>
2022-07-30 20:49:39 +0200

    Fixing layout media queries

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -4,14 +4,19 @@ const postcssPresetEnv = require('postcss-preset-env');
 const cssnano = require('cssnano');
 
 const config = {
-	plugins: [
-		//Some plugins, like tailwindcss/nesting, need to run before Tailwind,
-		tailwindcss(),
-		//But others, like autoprefixer, need to run after,
-		postcssShortVar,
-		postcssPresetEnv({ stage: 0 }),
-		cssnano,
-	],
+  plugins: [
+    postcssShortVar,
+    postcssPresetEnv({
+      stage: 0,
+      importFrom: './src/app.css',
+      // TODO: stay alert to future importFrom deprecation
+      features: { 'custom-properties': { disableDeprecationNotice: true } },
+    }),
+    //Some plugins, like tailwindcss/nesting, need to run before Tailwind,
+    tailwindcss(),
+    //But others, like autoprefixer, need to run after,
+    cssnano,
+  ],
 };
 
 module.exports = config;
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,46 +1,66 @@
 <script>
-	import Header from './_Header.svelte';
-	import Menu from './_Menu.svelte';
-	import '@fontsource/ubuntu/400.css';
-	import '@fontsource/ubuntu/500.css';
-	import '@fontsource/ubuntu/700.css';
-	import '../app.css';
-	import { currentRoute } from '../scripts/routes';
+  import Header from './_Header.svelte';
+  import Menu from './_Menu.svelte';
+  import '@fontsource/ubuntu/400.css';
+  import '@fontsource/ubuntu/500.css';
+  import '@fontsource/ubuntu/700.css';
+  import '../app.css';
+  import { currentRoute } from '../scripts/routes';
 </script>
 
 <svelte:head>
-	<title>{$currentRoute?.name} ∙ Theehuis</title>
+  <title>{$currentRoute?.name} ∙ Theehuis</title>
 </svelte:head>
 
 <Header />
 <main>
-	<Menu />
-	<section>
-		<slot />
-	</section>
+  <nav>
+    <Menu />
+  </nav>
+  <section>
+    <slot />
+  </section>
 </main>
 
 <style>
-	.contain {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
+  .contain {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
 
-	main {
-		max-width: --max-width;
-		margin-inline: auto;
-		@media (--sm) {
-			margin-block: 0;
-		}
-	}
+  main {
+    max-width: --max-width;
+    margin-inline: auto;
+    @media (--sm) {
+      margin-block: 0;
+    }
+  }
 
-	section {
-		width: 100%;
-		padding: 6rem --contain-padding;
-		@media (--sm) {
-			padding-left: calc(--contain-padding + --gap + --menu-width);
-			padding-block: 6rem 2rem;
-		}
-	}
+  nav {
+    font-weight: bold;
+    border-top: 1px solid #ddd;
+    position: fixed;
+    z-index: 2;
+    bottom: 0;
+    background-color: white;
+    width: 100%;
+    padding: 0.6rem --contain-padding;
+    @media (--sm) {
+      top: 0;
+      border-top: none;
+      border-right: 1px solid #ddd;
+      padding-block: 6rem;
+      width: --menu-width;
+    }
+  }
+
+  section {
+    width: 100%;
+    padding: 6rem --contain-padding;
+    @media (--sm) {
+      padding-left: calc(--contain-padding + --gap + --menu-width);
+      padding-block: 6rem 2rem;
+    }
+  }
 </style>
